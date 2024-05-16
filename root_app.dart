046f71bbf57475bace9988906c3a9463d5ad101f
utkt24 <167529720+utkt24@users.noreply.github.com>
2024-05-17 01:53:14 +0530

    Create root_app.dart

---
+++ root_app.dart
@@ -0,0 +1,91 @@
+import 'package:fitness_ui_kit/colors.dart';
+import 'package:fitness_ui_kit/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:line_icons/line_icon.dart';
+import 'package:line_icons/line_icons.dart';
+
+class RootApp extends StatefulWidget {
+  const RootApp({Key?key}) : super(key: key);
+
+  @override
+   _RootAppState createState() => _RootAppState();
+}
+
+class _RootAppState extends State<RootApp> {
+  int pageIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: getBody(),
+      bottomNavigationBar: getFooter(),
+    );
+  }
+
+  getBody() {
+    return IndexedStack(
+      index: pageIndex,
+      children: [
+       HomePage(),
+        Center(
+          child: Text("Chart"),
+        ),
+        Center(
+          child: Text("Camera"),
+        ),
+        Center(
+          child: Text("Profile"),
+        )
+      ],
+    );
+  }
+
+  getFooter() {
+    List items = [
+      LineIcons.home,
+      LineIcons.pieChart,
+      LineIcons.camera,
+      LineIcons.user,
+    ];
+    return Container(
+      height: 90,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(
+          top: BorderSide(width: 1,color: Colors.black.withOpacity(0.06))
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 20,right: 20,bottom: 20,top: 15),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: List.generate(items.length, (index) {
+            return InkWell(
+              onTap: (){
+                setState(() {
+                  pageIndex = index;
+                });
+              },
+              child: Column(
+                children: [
+                  Icon(items[index],size: 28,color: pageIndex == index ? thirdColor : Colors.black,),
+                  SizedBox(height: 5,),
+                  pageIndex == index ? Container(
+                    height: 6,
+                    width: 6,
+                    decoration: BoxDecoration(
+                        color: thirdColor,
+                        shape: BoxShape.circle
+                    ),
+                  ) : Container()
+                ],
+              ),
+            );
+
+          }),
+
+        ),
+      ),
+    );
+  }
+}
